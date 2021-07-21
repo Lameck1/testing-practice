66d97b4f953e989cb62b1979bb75c3f5340c57fe
Lameck1 <lameck72@gmail.com>
2021-07-21 10:22:50 +0300

    Write test for #stringLength added condition

--- test/stringLength.test.js
+++ test/stringLength.test.js
@@ -1,7 +1,17 @@
 import stringLength from "../js/stringLength";
 
-const str = 'Test String';
+const str = 'Some test';
+const emptyStr = '';
+const longStr = 'This is a very long string';
 
-test('counts length of string to equal 11', () => {
-  expect(stringLength(str)).toBe(11);
+test("counts length of 'str' to equal 11", () => {
+  expect(stringLength(str)).toBe(9);
+});
+
+test('throws an error if string has characters less that 1', () => {
+  expect(() => stringLength(emptyStr)).toThrow('String length out of bound.');
+});
+
+test('throws an error if string has characters more that 10', () => {
+  expect(() => stringLength(longStr)).toThrow('String length out of bound.');
 });
